502f57c51e9ffa5f8ccaf9d7c900f821df295352
surasint <isylzjko>
2017-03-31 22:06:50 +0200

    rearrage code

---
+++ maven/maven3_project_dependency/projectA/src/main/java/com/surasint/maven/maven02/projectA/ProjectAClass01.java
@@ -0,0 +1,10 @@
+package com.surasint.maven.maven02.projectA;
+
+/**
+ * Created by surasint on 1/4/2017.
+ */
+public class ProjectAClass01 {
+    public void test1(){
+        System.out.println("hi from projectA v2");
+    }
+}
---
+++ maven/maven3_project_dependency/projectB/src/main/java/com/surasint/maven/maven02/projectB/TestClass1.java
@@ -0,0 +1,13 @@
+package com.surasint.maven.maven02.projectB;
+
+import com.surasint.maven.maven02.projectA.ProjectAClass01;
+
+/**
+ * Created by surasint on 1/4/2017.
+ */
+public class TestClass1 {
+    public static void main(String[] args) {
+        ProjectAClass01 projectAClass01 = new ProjectAClass01();
+        projectAClass01.test1();
+    }
+}
