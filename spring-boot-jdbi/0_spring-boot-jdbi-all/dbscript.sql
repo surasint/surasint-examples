9cb23044be5ac27c6adf4860967071a583f4c577
surasint <isylzjko>
2017-12-11 20:59:27 +0100

    add git all

---
+++ spring-boot-jdbi/0_spring-boot-jdbi-all/dbscript.sql
@@ -0,0 +1,5 @@
+CREATE TABLE users (
+  id               INT PRIMARY KEY AUTO_INCREMENT,
+  username             VARCHAR(100) NOT NULL
+);
+
---
+++ spring-boot-jdbi/0_spring-boot-jdbi-all/src/main/java/com/surasint/example/service/UserService.java
@@ -0,0 +1,55 @@
+package com.surasint.example.service;
+
+import com.surasint.example.db.UserBean;
+import com.surasint.example.db.UserDao;
+import com.surasint.example.db.UserDao2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class UserService {
+
+
+    private UserDao userDao;
+    private UserDao2 userDao2;
+
+    @Autowired
+    UserService(UserDao userDao, UserDao2 userDao2){
+        this.userDao = userDao;
+        this.userDao2 = userDao2;
+    }
+
+    @Transactional
+    public List<UserBean> listTwoTimes(){
+        System.out.println(userDao.list());
+        return userDao.list();
+    }
+
+    @Transactional
+    public List<UserBean> listTwoDaos(){
+        System.out.println(userDao.list());
+        return userDao2.list();
+    }
+
+    @Transactional
+    public Integer insertOK(){
+        UserBean test = new UserBean();
+        test.setUsername("username"+new Date().getTime());
+
+        return userDao.insert(test);
+    }
+
+    @Transactional
+    public void insertAndFail(){
+        UserBean test = new UserBean();
+        test.setUsername("username"+new Date().getTime());
+
+        userDao.insert(test);
+        throw new RuntimeException("test fail");
+    }
+
+}
