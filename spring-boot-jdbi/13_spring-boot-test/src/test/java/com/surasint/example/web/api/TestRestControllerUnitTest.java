0ba97856f3d39633d33cb2583264c0e92cb865ca
surasint <isylzjko>
2018-02-14 20:45:38 +0100

    add another example how to test

---
+++ spring-boot-jdbi/13_spring-boot-test/src/test/java/com/surasint/example/web/api/TestRestControllerUnitTest.java
@@ -0,0 +1,33 @@
+package com.surasint.example.web.api;
+
+import com.surasint.example.service.CountService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestRestControllerUnitTest {
+
+    @Mock
+    private CountService mockCountService;
+
+    @InjectMocks
+    private TestRestController target;
+
+    @Test
+    public void test_count(){
+        //given
+        when(mockCountService.count()).thenReturn(100);
+
+        //when
+        String result = target.count();
+
+        //then
+        assertEquals("Count result:100", result);
+    }
+}
