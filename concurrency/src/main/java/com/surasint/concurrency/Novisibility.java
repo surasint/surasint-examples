bea1a5958606015607c7eb4cea9f69b5b4a444c1
surasint <isylzjko>
2019-10-23 20:16:37 +0200

    add example Novisibility.java

---
+++ concurrency/src/main/java/com/surasint/concurrency/Novisibility.java
@@ -0,0 +1,20 @@
+package com.surasint.concurrency;
+
+public class Novisibility {
+    private static boolean ready;
+    private static int number;
+
+    private static class ReaderThread extends Thread{
+        public void run() {
+            while (!ready)
+                Thread.yield();
+            System.out.println(number);
+        }
+    }
+
+    public static void main(String[] args) {
+        new ReaderThread().start();
+        number = 42;
+        ready = true;
+    }
+}
